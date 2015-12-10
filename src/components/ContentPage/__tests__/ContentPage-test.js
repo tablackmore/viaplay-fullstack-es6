9a0f0a46d9699f98b7aaffd6c49d73ca2f0d39dc
Tom Blackmore <tablackmore@gmail.com>
2015-12-10 08:28:37 +0100

    formatting

--- src/components/ContentPage/__tests__/ContentPage-test.js
+++ src/components/ContentPage/__tests__/ContentPage-test.js
@@ -8,37 +8,37 @@ const TestUtils = React.addons.TestUtils;
 describe('ContentPage', function cb() {
   it('should set header with correct label for each field, plus an empty header for actions', function cbIt() {
     const film = {
-        content: {
-          title: 'Pulp fiction',
-          production: {
-            year: 1997,
-            country: 'USA',
-          },
-          duration: {
-            readable: '1hr 52mins',
-          },
-          imdb: {
-            url: 'http://imdb.org/test',
-            rating: 4.5,
-            votes: 5679,
-          },
-          synopsis: 'crazy violent mad',
-          people: {
-            actors: ['Fred Perry', 'Tom Blackmore'],
-            directors: ['Inga Ivardsson'],
-          },
-          parentalRating: '15',
+      content: {
+        title: 'Pulp fiction',
+        production: {
+          year: 1997,
+          country: 'USA',
         },
-        _links: {
-          'viaplay:genres': [{
-            href: 'http://somesite.com/comedy',
-            title: 'comedy',
-          }, {
-            href: 'http://somesite.com/horror',
-            title: 'horror',
-          }],
+        duration: {
+          readable: '1hr 52mins',
         },
-      };
+        imdb: {
+          url: 'http://imdb.org/test',
+          rating: 4.5,
+          votes: 5679,
+        },
+        synopsis: 'crazy violent mad',
+        people: {
+          actors: ['Fred Perry', 'Tom Blackmore'],
+          directors: ['Inga Ivardsson'],
+        },
+        parentalRating: '15',
+      },
+      _links: {
+        'viaplay:genres': [{
+          href: 'http://somesite.com/comedy',
+          title: 'comedy',
+        }, {
+          href: 'http://somesite.com/horror',
+          title: 'horror',
+        }],
+      },
+    };
 
     const trailer = {
       trailer_id: 77789,
